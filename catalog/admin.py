15acc6b0e1f05e796126b1afef8f59c507dbc48d
YahirGm1123 <Yahirgarcia1123@gmail.com>
2026-05-22 00:55:44 -0600

    Enhance BookInstance admin with borrower and filters
    Added 'borrower' to the BookInstance display list and improved filtering and organization in the admin interface.

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,7 +1,6 @@
 from django.contrib import admin
 from .models import Author, Genre, Book, BookInstance
 
-# Registra el género de forma simple
 admin.site.register(Genre)
 
 class AuthorAdmin(admin.ModelAdmin):
@@ -15,13 +14,11 @@ class BookAdmin(admin.ModelAdmin):
 
 @admin.register(BookInstance)
 class BookInstanceAdmin(admin.ModelAdmin):
-    # NUEVO: Añadimos 'borrower' a la lista que se ve en la tabla
+
     list_display = ('book', 'status', 'borrower', 'due_back', 'id')
-    
-    # NUEVO: Filtros útiles para buscar libros rápidamente
+
     list_filter = ('status', 'due_back')
     
-    # NUEVO: Organizamos las cajas de texto al editar una copia
     fieldsets = (
         (None, {
             'fields': ('book', 'imprint', 'id')
